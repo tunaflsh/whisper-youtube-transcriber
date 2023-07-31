15f041068381c60884882b0a4a0b23f029e2add3
tunaflsh <bumtuan2001@gmail.com>
2023-07-31 04:51:31 +0200

    Move get_json_skeleton to utils.py

--- tagger.py
+++ tagger.py
@@ -2,7 +2,6 @@ import argparse
 import os
 import subprocess
 import json
-import re
 
 
 parser = argparse.ArgumentParser(description='Tagger')
@@ -22,55 +21,12 @@ def extract_metadata(file_path):
 
 
 metadata = extract_metadata(args.file)
-
-
-def get_json_skeleton(data, root=True):
-    if type(data) == dict:
-        return {
-            k: get_json_skeleton(v) for k, v in data.items()
-        }
-    if type(data) != list:
-        return type(data).__name__
-    if any(type(item) != type(data[0]) for item in data):
-        raise ValueError("List items must be of the same type")
-
-    skeleton = {}
-    if type(data[0]) == dict:
-        common_keys = set.intersection(*[set(k for k in item if not re.match(r'\[.*\]', k))
-                                         for item in data])
-        optional_keys = set.union(*[set(item.keys())
-                                  for item in data]) - common_keys
-        if root:
-            skeleton['len'] = len(data)
-        skeleton['items'] = {}
-        skeleton['items'].update({
-            k: get_json_skeleton([item[k] for item in data], root=False)
-            for k in common_keys
-        })
-        skeleton['items'].update({
-            re.sub(r'\[?([^\[\]]+)\]?', '[\1]', k):
-                get_json_skeleton([item[k] for item in data], root=False)
-            for k in optional_keys
-        })
-        return skeleton
-    if type(data[0]) == list:
-        lengths = [len(item) for item in data]
-        if root:
-            skeleton['len'] = len(data)
-        skeleton['range'] = (min(lengths), max(lengths))
-        skeleton['items'] = get_json_skeleton(
-            [x for item in data for x in item], root=False
-        )
-        return skeleton
-
-    if not root:
-        return type(data[0]).__name__
-    skeleton['len'] = len(data)
-    skeleton['items'] = type(data[0]).__name__
-    return skeleton
-
-
-url = metadata['format']['tags']['PURL']
+try:
+    url = metadata['format']['tags']['PURLf']
+except KeyError as e:
+    file = args.file.replace('"', '\\"')
+    print(f'KeyError: {e} in the metadata of "{file}"')
+    url = None
 
 with open(json_file) as f:
     transcript = json.load(f)
@@ -84,8 +40,9 @@ for segment in transcript['segments']:
         f"{h:d}:{m:02d}:{s:02d}" if h
         else f"{m:d}:{s:02d}"
     )
+    timestamped_url = f"{url}&t={timestamp}s" if url else ''
     text = segment['text']
-    tags.append(f"[{formatted_timestamp}]({url}&t={timestamp}s) {text}")
+    tags.append(f"[{formatted_timestamp}]({timestamped_url}) {text}")
 
 with open(tag_file, 'w') as f:
     print(*tags, sep='\\\n', file=f, end='\n')
---
+++ utils.py
@@ -0,0 +1,47 @@
+import re
+
+
+def get_json_skeleton(data, root=True):
+    if type(data) == dict:
+        return {
+            k: get_json_skeleton(v) for k, v in data.items()
+        }
+    if type(data) != list:
+        return type(data).__name__
+    if any(type(item) != type(data[0]) for item in data):
+        raise ValueError("List items must be of the same type")
+
+    skeleton = {}
+    if type(data[0]) == dict:
+        common_keys = set.intersection(*[set(k for k in item if not re.match(r'\[.*\]', k))
+                                         for item in data])
+        optional_keys = set.union(*[set(item.keys())
+                                  for item in data]) - common_keys
+        if root:
+            skeleton['len'] = len(data)
+        skeleton['items'] = {}
+        skeleton['items'].update({
+            k: get_json_skeleton([item[k] for item in data], root=False)
+            for k in common_keys
+        })
+        skeleton['items'].update({
+            re.sub(r'\[?([^\[\]]+)\]?', '[\1]', k):
+                get_json_skeleton([item[k] for item in data], root=False)
+            for k in optional_keys
+        })
+        return skeleton
+    if type(data[0]) == list:
+        lengths = [len(item) for item in data]
+        if root:
+            skeleton['len'] = len(data)
+        skeleton['range'] = (min(lengths), max(lengths))
+        skeleton['items'] = get_json_skeleton(
+            [x for item in data for x in item], root=False
+        )
+        return skeleton
+
+    if not root:
+        return type(data[0]).__name__
+    skeleton['len'] = len(data)
+    skeleton['items'] = type(data[0]).__name__
+    return skeleton
