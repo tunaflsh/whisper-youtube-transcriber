79626d794667c781ea94b871f90fb634af97d05d
tunaflsh <bumtuan2001@gmail.com>
2023-07-31 05:11:56 +0200

    Format with black formatter

--- tagger.py
+++ tagger.py
@@ -4,8 +4,8 @@ import subprocess
 import json
 
 
-parser = argparse.ArgumentParser(description='Tagger')
-parser.add_argument('file', type=str, help='Audio file to tag')
+parser = argparse.ArgumentParser(description="Tagger")
+parser.add_argument("file", type=str, help="Audio file to tag")
 args = parser.parse_args()
 
 basename = os.path.basename(args.file)
@@ -16,13 +16,23 @@ tag_file = f"timestamps/{basename}.md"
 
 def extract_metadata(file_path):
     meta_data = subprocess.check_output(
-        ["ffprobe", "-v", "quiet", "-print_format", "json", "-show_format", "-show_streams", file_path])
+        [
+            "ffprobe",
+            "-v",
+            "quiet",
+            "-print_format",
+            "json",
+            "-show_format",
+            "-show_streams",
+            file_path,
+        ]
+    )
     return json.loads(meta_data)
 
 
 metadata = extract_metadata(args.file)
 try:
-    url = metadata['format']['tags']['PURLf']
+    url = metadata["format"]["tags"]["PURLf"]
 except KeyError as e:
     file = args.file.replace('"', '\\"')
     print(f'KeyError: {e} in the metadata of "{file}"')
@@ -32,17 +42,14 @@ with open(json_file) as f:
     transcript = json.load(f)
 
 tags = []
-for segment in transcript['segments']:
-    timestamp = int(segment['start'])
+for segment in transcript["segments"]:
+    timestamp = int(segment["start"])
     m, s = divmod(timestamp, 60)
     h, m = divmod(m, 60)
-    formatted_timestamp = (
-        f"{h:d}:{m:02d}:{s:02d}" if h
-        else f"{m:d}:{s:02d}"
-    )
-    timestamped_url = f"{url}&t={timestamp}s" if url else ''
-    text = segment['text']
+    formatted_timestamp = f"{h:d}:{m:02d}:{s:02d}" if h else f"{m:d}:{s:02d}"
+    timestamped_url = f"{url}&t={timestamp}s" if url else ""
+    text = segment["text"]
     tags.append(f"[{formatted_timestamp}]({timestamped_url}) {text}")
 
-with open(tag_file, 'w') as f:
-    print(*tags, sep='\\\n', file=f, end='\n')
+with open(tag_file, "w") as f:
+    print(*tags, sep="\\\n", file=f, end="\n")
